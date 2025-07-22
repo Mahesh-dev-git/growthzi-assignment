a2fc3ff821a495735b5bec2146107cf229bf1482
Mahesh <maheshsk0470@gmail.com>
2025-07-22 19:10:30 +0530

    Integrated backend API and completed edit functionality

--- src/components/RoomCategory.jsx
+++ src/components/RoomCategory.jsx
@@ -32,7 +32,7 @@ const RoomCategory = () => {
 
   const handleSave = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/update-section", {
+      const response = await fetch("https://flask-backend-u613.onrender.com/update-section", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
